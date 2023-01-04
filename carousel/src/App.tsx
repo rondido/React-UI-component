b7c96181eddbae6019818bf05b284fab20eaed9f
rondido <rppr001@naver.com>
2023-01-04 22:34:28 +0900

    React Casousel commit

---
+++ carousel/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Carousel from './components/Carousel';
+
+function App() {
+  return (
+    <div className="App">
+      <Carousel/>
+    </div>
+  );
+}
+
+export default App;
